993966f2745492005a99353b14b562b17573a9a3
NekoBend <iamneckbend@gmail.com>
2024-09-24 19:47:42 +0900

    Refactor nekobend_utils.py to add utility methods for string manipulation

--- nekobend_utils.py
+++ nekobend_utils.py
@@ -1,7 +1,15 @@
-import os
-import sys
+import asyncio
+import functools
 import json
+import queue
+import re
 import subprocess
+import threading
+import time
+
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from datetime import datetime
+from typing import Callable, Union, List, Any, Iterator
 
 
 class PwshRequests:
@@ -24,4 +32,43 @@ class Clipboard:
 
 
 class ReString(str):
-    pass
+
+    def __new__(cls, value: str) -> 'ReString':
+        return super().__new__(cls, value)
+
+    def __str__(self) -> str:
+        return self
+
+    def __repr__(self) -> str:
+        return self
+
+    def to_string(self) -> str:
+        return str(self)
+
+    def match(self, pattern: str, flags=0) -> Union[re.Match[str], None]:
+        return re.match(pattern, self, flags)
+
+    def fullmatch(self, pattern: str, flags=0) -> Union[re.Match[str], None]:
+        return re.fullmatch(pattern, self, flags)
+
+    def search(self, pattern: str, flags=0) -> Union[re.Match[str], None]:
+        return re.search(pattern, self, flags)
+
+    def sub(self, pattern: str, repl: str, count: int = 0, flags=0) -> 'ReString':
+        result = re.sub(pattern, repl, self, count, flags)
+        return ReString(result)
+
+    def subn(self, pattern: str, repl: str, count: int = 0, flags=0) -> tuple['ReString', int]:
+        result, num_subs = re.subn(pattern, repl, self, count, flags)
+        return ReString(result), num_subs
+
+    def rsplit(self, pattern: str, maxsplit: int = 0, flags=0) -> List[Union['ReString', Any]]:
+        results = re.split(pattern, self, maxsplit, flags)
+        return [ReString(item) for item in results if isinstance(item, str)]
+
+    def findall(self, pattern: str, flags=0) -> List[Union[Any]]:
+        results = re.findall(pattern, self, flags)
+        return [ReString(item) for item in results if isinstance(item, str)]
+
+    def finditer(self, pattern: str, flags=0) -> Iterator[re.Match[str]]:
+        return re.finditer(pattern, self, flags)
