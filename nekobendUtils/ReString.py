bd6439d971c84d434f4e0d549d641e16515b4fb7
NekoBend <iamneckbend@gmail.com>
2024-10-31 19:56:14 +0900

    Refactor ReString methods to use instance string for regex operations

--- nekobendUtils/ReString.py
+++ nekobendUtils/ReString.py
@@ -17,38 +17,34 @@ class ReString(str):
     def to_string(self) -> str:
         return str(self)
 
-    def match(self, pattern: str, string: str, flags=0) -> Union[re.Match[str], None]:
-        return re.match(pattern, string, flags)
+    def match(self, pattern: str, flags=0) -> Union[re.Match[str], None]:
+        return re.match(pattern, self, flags)
 
-    def fullmatch(
-        self, pattern: str, string: str, flags=0
-    ) -> Union[re.Match[str], None]:
-        return re.fullmatch(pattern, string, flags)
+    def fullmatch(self, pattern: str, flags=0) -> Union[re.Match[str], None]:
+        return re.fullmatch(pattern, self, flags)
 
-    def search(self, pattern: str, string: str, flags=0) -> Union[re.Match[str], None]:
-        return re.search(pattern, string, flags)
+    def search(self, pattern: str, flags=0) -> Union[re.Match[str], None]:
+        return re.search(pattern, self, flags)
 
-    def sub(
-        self, pattern: str, repl: str, string: str, count: int = 0, flags=0
-    ) -> "ReString":
-        result = re.sub(pattern, repl, string, count, flags)
+    def sub(self, pattern: str, repl: str, count: int = 0, flags=0) -> "ReString":
+        result = re.sub(pattern, repl, self, count, flags)
         return ReString(result)
 
     def subn(
-        self, pattern: str, repl: str, string: str, count: int = 0, flags=0
+        self, pattern: str, repl: str, count: int = 0, flags=0
     ) -> tuple["ReString", int]:
-        result, num_subs = re.subn(pattern, repl, string, count, flags)
+        result, num_subs = re.subn(pattern, repl, self, count, flags)
         return ReString(result), num_subs
 
     def resplit(
-        self, pattern: str, string: str, maxsplit: int = 0, flags=0
+        self, pattern: str, maxsplit: int = 0, flags=0
     ) -> List[Union["ReString", Any]]:
-        results = re.split(pattern, string, maxsplit, flags)
+        results = re.split(pattern, self, maxsplit, flags)
         return [ReString(item) for item in results if isinstance(item, str)]
 
-    def findall(self, pattern: str, string: str, flags=0) -> List[Union[Any]]:
-        results = re.findall(pattern, string, flags)
+    def findall(self, pattern: str, flags=0) -> List[Union[Any]]:
+        results = re.findall(pattern, self, flags)
         return [ReString(item) for item in results if isinstance(item, str)]
 
-    def finditer(self, pattern: str, string: str, flags=0) -> Iterator[re.Match[str]]:
-        return re.finditer(pattern, string, flags)
+    def finditer(self, pattern: str, flags=0) -> Iterator[re.Match[str]]:
+        return re.finditer(pattern, self, flags)
