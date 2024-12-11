a0995094f1e41c840f14401906128f7c0c92f64b
NekoBend <iamneckbend@gmail.com>
2024-12-11 12:02:15 +0900

    __all__リストからエイリアス定義を削除

--- src/__init__.py
+++ src/__init__.py
@@ -3,10 +3,5 @@ from .CmdObserver import CmdObserver
 from .PwshRequests import PwshRequests
 from .ReString import ReString
 
-# エイリアスを定義
-Clipboard = Clipboard
-CmdObserver = CmdObserver
-PwshRequests = PwshRequests
-ReString = ReString
 
 __all__ = ["Clipboard", "CmdObserver", "PwshRequests", "ReString"]
